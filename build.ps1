9121fbc97d2c37b868aff6f410f2f838e36c123c
CookieHCl <nhc7502@snu.ac.kr>
2026-01-19 03:53:40 +0900

    feat(MFAA): Set default language of MFAAvalonia to English

--- build.ps1
+++ build.ps1
@@ -1,4 +1,7 @@
-Remove-Item -Recurse -Force .\install\*
-python ./install.py
-Copy-Item -Recurse -Force .\deps\MFAAvalonia\* .\install\
+if (Test-Path -Path '.\install') {
+    Remove-Item -Recurse -Force '.\install\*'
+}
+python '.\install.py'
+Copy-Item -Recurse -Force '.\deps\MFAAvalonia\*' '.\install\'
+New-Item -Path '.\install\config\config.json' -Value '{"CurrentLanguage": "en-US"}' -Force
 .\install\MFAAvalonia.exe
