545108340782d04cfb97f4dbbc7f7e7021047061
CookieHCl <nhc7502@snu.ac.kr>
2026-01-19 05:21:29 +0900

    fix(MFAA): Fix typo in MFAAvalonia option

--- build.ps1
+++ build.ps1
@@ -3,5 +3,5 @@ if (Test-Path -Path '.\install') {
 }
 python '.\install.py'
 Copy-Item -Recurse -Force '.\deps\MFAAvalonia\*' '.\install\'
-New-Item -Path '.\install\config\config.json' -Value '{"CurrentLanguage":"en-US,"CurrentTasks":["리세마라<|||>Main"]"}' -Force
+New-Item -Path '.\install\config\config.json' -Value '{"CurrentLanguage":"en-US","CurrentTasks":["리세마라<|||>Main"]}' -Force
 .\install\MFAAvalonia.exe
